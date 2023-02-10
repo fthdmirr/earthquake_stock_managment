381412296ed36b404659cbdd8616db44cac1d282
Fatih Demir <fthdmirr@gmail.com>
2023-02-11 01:59:14 +0300

    added views

--- lib/view/receive/screen/receiving_view.dart
+++ lib/view/receive/screen/receiving_view.dart
@@ -1,3 +1,4 @@
+import 'package:earhquake_stock_managment/core/common/models/receive_model.dart';
 import 'package:earhquake_stock_managment/core/common/provider/view_model_provider.dart';
 import 'package:earhquake_stock_managment/core/components/appbar/base_app_bar.dart';
 import 'package:earhquake_stock_managment/core/components/dropdown/dropdown_input.dart';
@@ -17,24 +18,98 @@ class ReceivingView extends StatelessWidget {
             title: 'Mal Kabul',
           ),
           body: SingleChildScrollView(
-              child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    const Text('Gelen İl:  '),
-                    Expanded(
-                      child: DropdownInput<String>(
-                        dropdownValues: CitiesOfTurkey.values.map((e) => e.name).toList(),
-                        firstValue: CitiesOfTurkey.kayseri.name,
-                      ),
-                    ),
-                  ],
-                )
-              ],
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  DropdownAndTitleWidget(
+                    title: 'Araç Tipi',
+                    dropdownList: Vehicle.values.map((e) => e.name).toList(),
+                    dropDownFirstValue: Vehicle.kamyon.name,
+                  ),
+                  const BaseInput(
+                    title: 'Araç Plakasi',
+                    hint: 'Lütfen araç plakası giriniz',
+                  ),
+                  DropdownAndTitleWidget(
+                    title: 'Gelen İl',
+                    dropdownList: CitiesOfTurkey.values.map((e) => e.name).toList(),
+                    dropDownFirstValue: CitiesOfTurkey.kayseri.name,
+                  ),
+                  DropdownAndTitleWidget(
+                    title: 'Ürün',
+                    dropdownList: CitiesOfTurkey.values.map((e) => e.name).toList(),
+                    dropDownFirstValue: CitiesOfTurkey.kayseri.name,
+                  ),
+                  DropdownAndTitleWidget(
+                    title: 'Ürün Tipi',
+                    dropdownList: CitiesOfTurkey.values.map((e) => e.name).toList(),
+                    dropDownFirstValue: CitiesOfTurkey.kayseri.name,
+                  ),
+                ],
+              ))),
+    );
+  }
+}
+
+class BaseInput extends StatelessWidget {
+  const BaseInput({
+    super.key,
+    required this.title,
+    required this.hint,
+  });
+
+  final String title;
+  final String hint;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(child: Text('$title:   ')),
+        Expanded(
+          flex: 2,
+          child: TextFormField(
+            controller: TextEditingController(),
+            decoration: InputDecoration(
+              hintText: hint,
+              border: const OutlineInputBorder(),
             ),
-          ))),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class DropdownAndTitleWidget extends StatelessWidget {
+  const DropdownAndTitleWidget({
+    super.key,
+    required this.title,
+    required this.dropdownList,
+    this.dropDownFirstValue,
+  });
+
+  final String title;
+  final List<String> dropdownList;
+  final String? dropDownFirstValue;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(child: Text('$title:   ')),
+            Expanded(
+              flex: 3,
+              child: DropdownInput<String>(
+                dropdownValues: dropdownList,
+                firstValue: dropDownFirstValue,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 16),
+      ],
     );
   }
 }
