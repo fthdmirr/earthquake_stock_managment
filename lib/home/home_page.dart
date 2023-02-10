87402b02961c8cd4951ef75c3d6833f574dcdd31
Fatih Demir <fthdmirr@gmail.com>
2023-02-11 00:52:49 +0300

    example of provider

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,7 @@
+import 'package:earhquake_stock_managment/core/common/provider/view_model_provider.dart';
 import 'package:earhquake_stock_managment/core/components/dropdown/dropdown_input.dart';
 import 'package:earhquake_stock_managment/core/utils/constants/cities_and_districts.dart';
+import 'package:earhquake_stock_managment/home/home_view_model.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,8 +9,9 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
+    return ViewModelProvider<HomeViewModel>(
+      model: HomeViewModel(context: context),
+      builder: (model) => Column(
         children: [
           const SizedBox(height: 40),
           DropdownInput<String>(
---
+++ lib/home/home_view_model.dart
@@ -0,0 +1,13 @@
+
+import 'package:earhquake_stock_managment/core/common/provider/base_provider.dart';
+
+class HomeViewModel extends BaseViewModel{
+  HomeViewModel({required super.context});
+
+
+  functions(){
+    
+  }
+
+
+}
