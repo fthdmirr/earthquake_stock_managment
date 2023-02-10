62bf30f5393d30cebff4b584361bf900eb02c97b
Fatih Demir <fthdmirr@gmail.com>
2023-02-11 00:53:41 +0300

    changed provider structure

--- lib/core/common/provider/view_model_provider.dart
+++ lib/core/common/provider/view_model_provider.dart
@@ -5,39 +5,28 @@ import 'package:provider/provider.dart';
 
 import 'base_provider.dart';
 
-enum ProvideMode {
-  CONSUME,
-  SELECT,
-}
-
 class ViewModelProvider<T extends BaseViewModel> extends StatelessWidget {
   final BaseViewModel model;
   final Widget Function(T model) builder;
-  final ProvideMode provideMode;
 
   const ViewModelProvider({
     super.key,
     required this.model,
     required this.builder,
-    this.provideMode = ProvideMode.CONSUME,
   });
 
   @override
   Widget build(BuildContext context) {
-    bool isConsume = (this.provideMode == ProvideMode.CONSUME);
     return ChangeNotifierProvider<T>(
-      lazy: true,
-      create: (BuildContext context) => model as T,
-      child: isConsume
-          ? Consumer<T>(
-              builder: (
-                BuildContext context,
-                T value,
-                Widget? child,
-              ) =>
-                  builder(value),
-            )
-          : builder(model as T),
-    );
+        lazy: true,
+        create: (BuildContext context) => model as T,
+        child: Consumer<T>(
+          builder: (
+            BuildContext context,
+            T value,
+            Widget? child,
+          ) =>
+              builder(value),
+        ));
   }
 }
