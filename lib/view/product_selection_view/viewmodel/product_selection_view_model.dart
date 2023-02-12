654836bffb72d83c6523531d81a5b5e22f5d038b
Fatih Demir <fthdmirr@gmail.com>
2023-02-12 10:40:33 +0300

    fixed some widgets

--- lib/view/product_selection_view/viewmodel/product_selection_view_model.dart
+++ lib/view/product_selection_view/viewmodel/product_selection_view_model.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:earhquake_stock_managment/core/common/models/inventory_item/inventory_item_model.dart';
 import 'package:earhquake_stock_managment/core/common/provider/base_provider.dart';
 import 'package:flutter/material.dart';
 
@@ -8,12 +9,9 @@ import '../../../core/utils/constants/enum/cities_of_turkey.dart';
 class ProductSelectionViewModel extends BaseViewModel {
   ProductSelectionViewModel({required super.context});
 
-  int productNumber = 0;
-
   final TextEditingController vehicleTypeController = TextEditingController();
   final TextEditingController vehicleNoController = TextEditingController();
-  final TextEditingController driverInformationController =
-      TextEditingController();
+  final TextEditingController driverInformationController = TextEditingController();
   final TextEditingController phoneNoController = TextEditingController();
 
   final String selectedVehicle = 'Kamyon';
@@ -23,13 +21,26 @@ class ProductSelectionViewModel extends BaseViewModel {
   final String selectedItemType = 'Koli';
   final TextEditingController quantity = TextEditingController();
 
-  increment() {
-    productNumber++;
+  final List<InventoryItem> products = [
+    InventoryItem(
+      name: 'Pantulon',
+      quantity: 10,
+    ),
+    InventoryItem(
+      name: 'Pantulon',
+      quantity: 10,
+    ),
+  ];
+
+  getProducts() {}
+
+  increment(InventoryItem item) {
+    item.quantity++;
     notifyListeners();
   }
 
-  decrement() {
-    productNumber = max(productNumber - 1, 0);
+  decrement(InventoryItem item) {
+    item.quantity = max(item.quantity - 1, 0);
     notifyListeners();
   }
 }
